ecf058820b11a39d7b0a65cab9595baa45476784
David Bishop <d.bishop@ctidigital.com>
2023-05-23 20:22:41 +0100

    New nav panel story

---
+++ apps/storybook/patterns/05-pages/nav-panel/nav-panel.stories.jsx
@@ -0,0 +1,22 @@
+import { RenderTwig } from '@wingsuit-designsystem/pattern-react';
+import React from 'react';
+import 'atoms/hamburger';
+import 'molecules/card';
+import 'atoms/button';
+import 'molecules/hero';
+import 'atoms/image';
+import 'organisms/header';
+import 'organisms/footer';
+
+export default {
+  title: 'Pages/Nav Panel',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+const template = require('./nav-panel.twig');
+
+export const Homepage = () => {
+  return <RenderTwig data={template}></RenderTwig>;
+};
